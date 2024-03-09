13e11189b0a3aaedd6b24d7193ea4ef7f25a94e6
Dhiraj <dkskp2005@gmail.com>
2024-03-09 11:03:10 +0530

    Small updates, v1.0.3

--- README.md
+++ README.md
@@ -4,8 +4,17 @@ It is a package that can simply Server Developement.
 
 Instead of writing lines of codes developers can use this package to simply connect mongoDB to express server.
 
+## Features
+
 Right now it can connect to mongodb database.
 
 ## Usage
 
-use ConnectMongoDB() and pass MONGOURI as an arg to it.
+### ConnectMongoDB
+
+'''
+ConnectToMongoDB("MongoDB URI");
+'''
+
+[!TIP]
+You can remove bugs/errors from this package or add new features to it [ Github ](https://github.com/dhiraj2105/men-pack)
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
+// import bcrypt from "bcrypt";
 
 // Connect mongoose with mongoURI
 const ConnectMongoDB = (MONGOURI) => {
   mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    connectTimeoutMS: 30000,
   });
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "MongoDB connection error"));
@@ -13,4 +15,4 @@ const ConnectMongoDB = (MONGOURI) => {
 };
 
 // EXPORT
-export { ConnectMongoDB };
+export { ConnectMongoDB, LoginAPI };
